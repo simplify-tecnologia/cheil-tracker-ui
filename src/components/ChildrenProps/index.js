71d215cbac1dfcfdcc50b0d300013b768c42b923
Cesar Denis <cesar@cesardenis.com>
2018-09-24 10:38:15 -0300

    feature: add children props component

---
+++ src/components/ChildrenProps/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ChildrenProps extends React.Component {
+  static displayName = 'ChildrenProps';
+
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    const { children, ...props } = this.props;
+
+    if (!children) return null;
+
+    const newChildren = React.Children.map(children, c => {
+      const p = { ...c.props, ...props };
+      return React.cloneElement(c, p);
+    });
+
+    return newChildren;
+  }
+}
+
+export default ChildrenProps;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
+export { default as ChildrenProps } from './ChildrenProps';
 export { default as Text } from './Text';
 export { default as Title } from './Title';
