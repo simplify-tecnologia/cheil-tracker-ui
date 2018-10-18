edd3c3659c5152f5e9e61520a62e368a2400084e
Cesar Denis <cesar@cesardenis.com>
2018-10-18 08:46:07 -0300

    feature: add loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { main } from 'cheil-tracker-ui/themes';
+import { compose } from 'recompose';
+import { withFallbackTheme, withVariant } from 'cheil-tracker-ui/hocs';
+import * as s from './styles';
+
+/**
+ * A **Loader** alerts a user to wait for an activity to complete.
+ */
+class Loader extends React.Component {
+  static displayName = 'Loader';
+
+  static defaultProps = {
+    size: '1.125em',
+    fontFamily: 'primary',
+    fontSize: 2,
+    p: null,
+    m: 0,
+    borderSize: 3,
+  };
+
+  static propTypes = {
+    /**
+     * A loader can have different colors. <br/>
+     * **Enum**: `primary`, `secondary`, `success`, `danger`, `white`, `black`, `grey`, `#FF0000`, ...
+     * */
+    color: PropTypes.string,
+    /**
+     * A loader can have different sizes. <br/>
+     * **Enum**: `tiny`, `small`, `normal`, `medium`, `large`, `xlarge`, `huge`
+     * */
+    size: PropTypes.string,
+    /** A loader can have it's color inverted */
+    inverted: PropTypes.bool,
+    /** A loader can display it's children **vertical**. */
+    vertical: PropTypes.bool,
+  };
+
+  render() {
+    const className = cx(`loader`, this.props.className);
+    const { children, ...rest } = this.props;
+
+    return (
+      <s.Loader {...rest} className={className}>
+        <s.LoaderContainer {...rest}>
+          <s.LoaderShadow {...rest} />
+          <s.LoaderCircle {...rest} />
+        </s.LoaderContainer>
+        {children && <div>{children}</div>}
+      </s.Loader>
+    );
+  }
+}
+
+export default compose(
+  withFallbackTheme(main),
+  withVariant('loader')
+)(Loader);
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as Badge } from './Badge';
 export { default as Card } from './Card';
 export { default as ChildrenProps } from './ChildrenProps';
 export { default as Icon } from './Icon';
+export { default as Loader } from './Loader';
 export { default as Text } from './Text';
 export { default as Title } from './Title';
